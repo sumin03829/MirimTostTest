0bc9ce3b0dc7af1c0cf032dbb3a6bf37cbe2acae
sumin_030829 <sumin0829@kako.com>
2020-11-12 20:37:21 +0900

    InComplete Dialog

--- app/src/main/java/kr/hs/emirim/s2019s38/mirimtosttest/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/s2019s38/mirimtosttest/MainActivity.java
@@ -1,5 +1,6 @@
 package kr.hs.emirim.s2019s38.mirimtosttest;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -17,18 +18,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button btn=findViewById(R.id.btn);
+        Button btnDiaglog=findViewById(R.id.btn_dialog);
         btn.setOnClickListener(btnListener);
+        btnDiaglog.setOnClickListener(btnListener);
     }
+
     View.OnClickListener btnListener=new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            Toast toast=Toast.makeText(getApplicationContext(),"토스트 연습",Toast.LENGTH_SHORT);
-            Display display=((WindowManager)getSystemService(WINDOW_SERVICE)).getDefaultDisplay(); //이 문장 기억하기
-            int xOffset=(int)(Math.random()*display.getWidth());
-            int yOffset=(int)(Math.random()*display.getHeight());
-            //toast.setGravity(Gravity.TOP|Gravity.LEFT,xOffset,yOffset);
-            toast.setGravity(Gravity.TOP|Gravity.CENTER_HORIZONTAL,0,70);
-            toast.show();
+            switch (view.getId()){
+                case R.id.btn:
+                    Toast toast=Toast.makeText(getApplicationContext(),"토스트 연습",Toast.LENGTH_SHORT);
+                    Display display=((WindowManager)getSystemService(WINDOW_SERVICE)).getDefaultDisplay(); //이 문장 기억하기
+                    int xOffset=(int)(Math.random()*display.getWidth());
+                    int yOffset=(int)(Math.random()*display.getHeight());
+                    //toast.setGravity(Gravity.TOP|Gravity.LEFT,xOffset,yOffset);
+                    toast.setGravity(Gravity.TOP|Gravity.CENTER_HORIZONTAL,0,70);
+                    toast.show();
+                    break;
+
+                case R.id.btn_dialog:
+                    AlertDialog.Builder dialog=new AlertDialog.Builder(getApplicationContext());
+                    dialog.setTitle("알림사항");
+                    dialog.setMessage("코로나 확진자 진행상황 입니다.");
+                    dialog.setIcon(R.mipmap.ic_flower2);
+                    dialog.setPositiveButton("확인",null);
+                    dialog.show();
+                    break;
+            }
+
         }
     };
 
